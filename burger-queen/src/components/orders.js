69dad1bdf80775a33581b6f928ac057b531f6d5e
betanyeli <betangelii@gmail.com>
2019-07-16 16:32:48 -0400

    adding firebase deploy and react route

---
+++ burger-queen/src/components/orders.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Orders() {
+    return (
+        <div className="orders">
+      
+          <h1>
+         Soy una orden
+          </h1>
+        
+
+            
+          
+  
+
+        </div>
+    );
+
+}
+
+export default Orders;
