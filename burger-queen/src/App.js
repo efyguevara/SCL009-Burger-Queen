b046f006ee796f164bea560144abac356e27cf7c
betanyeli <betangelii@gmail.com>
2019-07-17 18:21:55 -0400

    adding changes to master

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,38 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import NavBar from './components/navbar';
-import Waitress from './components/waitress';
-import Kitchen from './components/kitchen';
-import Orders from './components/orders';
-// import Button from './components/button';
-
+import NavBar from './components/elements/navbar';
+import Waitress from './components/views/waitress';
+import Kitchen from './components/views/kitchen';
+import Orders from './components/views/orders';
+// import Button from './components/elements/button';
+// <Button  buttonOnClick={ hazAlgo => console.log("He sido clickeado")}/>
 import './App.css';
+const menu = require('./data/foodOptions.json')
+console.log(menu)
+
 
 function App() {
 
   return (
     
-    
       <div className="App">
 <NavBar />
 <BrowserRouter>
 <Switch>
-<Route exact path="/waitress" component={Waitress} />
+ <Route exact path="/waitress" component={Waitress} />
 <Route exact path="/kitchen" component={Kitchen} />
 <Route exact path="/orders" component={Orders} />
 </Switch>
 </BrowserRouter>
 <div className="clients">
-<div className="btn-group" role="group" aria-label="Basic example">
-
-  <button type="button" className="btn btn-success">Meser@</button>
-  <button type="button" className="btn btn-success">Cocina</button>
-  <button type="button" className="btn btn-success">Órdenes listas</button>
-</div>
+<a href="./waitress" className="btn btn-success">Mesero</a>
+<a href="./kitchen" className="btn btn-success">Cocina</a>
+<a href="./orders" className="btn btn-success">órdenes listas</a>
 
 </div>
-
-
 
          </div>
 
