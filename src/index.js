abdb35771875e714b55237e332ce341d4e8c74c4
betanyeli <betangelii@gmail.com>
2019-07-26 01:58:35 -0400

    route to index.js !

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Waitress from './components/views/waitress';
+import WaitressLunch from './components/views/waitressLunch';
+import Kitchen from './components/views/kitchen';
+import Orders from './components/views/orders';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initRouter = (
+    <BrowserRouter>
+<Switch>
+<Route exact path= {process.env.PUBLIC_URL + '/'} component={Waitress} />
+ <Route exact path={process.env.PUBLIC_URL + '/waitress-breakfast'} component={Waitress} />
+ <Route exact path={process.env.PUBLIC_URL + '/waitress-lunch'} component={WaitressLunch} />
+<Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+<Route exact path={process.env.PUBLIC_URL + '/orders'} component={Orders} />
+</Switch>
+</BrowserRouter>
+)
+ReactDOM.render(initRouter, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
